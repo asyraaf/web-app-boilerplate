cd6b0afbd570e89d0b4f81807b3af2e4baf3e065
Nasrul Hazim Bin Mohamad <nasrulhazim@nasrulhazim.local>
2016-10-01 00:40:58 +0800

    added trainigs migration script

---
+++ database/migrations/2016_10_01_003737_create_trainings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTrainingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('creator_id');
+            $table->integer('trainer_id');
+            $table->integer('venue_id');
+            $table->date('date_start');
+            $table->date('date_end');
+            $table->time('time_start');
+            $table->time('time_end');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trainings');
+    }
+}
