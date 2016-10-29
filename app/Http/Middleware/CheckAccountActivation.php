c0510cb69286f6ab60113ec5e42bb38432b34a45
Nasrul Hazim <nasrulhazim.m@gmail.com>
2016-10-29 22:05:05 +0800

    handle API response on inactive account

--- app/Http/Middleware/CheckAccountActivation.php
+++ app/Http/Middleware/CheckAccountActivation.php
@@ -14,13 +14,17 @@ class CheckAccountActivation
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $api = 0)
     {
         if (Auth::user()->activation && Auth::user()->activation->status == 0) {
-            Auth::logout();
-            session()->flash('message', 'Please activate your account.');
-            session()->flash('error', 1);
-            return redirect('/');
+            if ($api) {
+                return response()->json(['error' => 'Account is not yet activated'], 401);
+            } else {
+                Auth::logout();
+                session()->flash('message', 'Please activate your account.');
+                session()->flash('error', 1);
+                return redirect('/');
+            }
         }
 
         return $next($request);
